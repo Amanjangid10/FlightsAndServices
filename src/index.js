9d78e03408c8bc252707a8ec37c78a279d1d82ff
Amanjangid10 <mailme.aman.jangid@gmail.com>
2023-09-18 08:21:27 +0530

    updated after cloning

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const {PORT}= require('./config/serverConfig')
 
-
-
+const bodyParser = require('body-parser')
 
 
 
 const SetupAndStartServer = async () => {
     
-    const app = express()
-  
-  
+    const app = express();
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
-    })
 
+    });
 }
-
 
 SetupAndStartServer()
