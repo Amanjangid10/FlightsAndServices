c23daccf59bf5a037b38f99f21436de8478925aa
Amanjangid10 <mailme.aman.jangid@gmail.com>
2023-09-22 10:19:40 +0530

    modified migrations settings and minor fixes

--- src/models/city.js
+++ src/models/city.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   City.init({
     name: DataTypes.STRING,
-    allowNull: false,
+    allowNull: false
   }, {
     sequelize,
     modelName: 'City',
